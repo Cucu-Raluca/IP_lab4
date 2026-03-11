ad921100baaa3e474ec653f544cfad17c17a472d
Apetrei Ionuț-Teodor <apetreionuteodor@gmail.com>
2026-03-11 19:02:27 +0200

    Update main function tests

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,11 @@
 import app.LoggedUser;
+import app.UnloggedUser;
 
 import java.util.List;
 
 public class Main
 {
     public static void main(String[] args) {
-
-        UnloggedUser user1=new UnloggedUser();
-
-        user1.viwePagina();
-        user1.appFunctionalitati();
-
-        System.out.println("Public events available:");
-        for(String pag: user1.browsePag())
-        {
-            System.out.println(pag);
-        }
-
-        user1.autentificareUser("Ana_are_mere","anamere67@gmail.ro");
-        user1.setupProfil("Palas","desenul");
         // 1. Instantiate the Logged User (Object)
         LoggedUser currentUser = new LoggedUser("USR-123", "AlexExplorer", "alex@example.com");
 
@@ -32,5 +19,20 @@ public class Main
 
         // 4. Call the important method (Initiate Planning)
         currentUser.initiateGroupPlanning("Weekend Rooftop Jazz Concert", friendsToInvite);
+
+        // 5. Test UnloggedUser class
+        UnloggedUser user1 = new UnloggedUser();
+
+        user1.viwePagina();
+        user1.appFunctionalitati();
+
+        System.out.println("Public events available:");
+        for(String pag: user1.browsePag())
+        {
+            System.out.println(pag);
+        }
+
+        user1.autentificareUser("Ana_are_mere","anamere67@gmail.ro");
+        user1.setupProfil("Palas","desenul");
     }
 }
